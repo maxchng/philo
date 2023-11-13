eb49dca4a185c6592b7ee83a9071eeae9315c857
Yuen Zheng Chng <ychng@u80z04s03.42kl.edu.my>
2023-11-13 17:02:19 +0800

    Add write_error function and usage messages

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 14:08:29 by ychng             #+#    #+#             */
-/*   Updated: 2023/11/13 14:11:24 by ychng            ###   ########.fr       */
+/*   Updated: 2023/11/13 16:59:00 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,8 @@
 
 # include "sys_headers.h"
 # include "libft.h"
+
+// philo.c
+void	write_error(char *msg);
 
 #endif
--- philo/philo.c
+++ philo/philo.c
@@ -6,12 +6,27 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 14:07:56 by ychng             #+#    #+#             */
-/*   Updated: 2023/11/13 14:11:58 by ychng            ###   ########.fr       */
+/*   Updated: 2023/11/13 16:57:17 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/philo.h"
 
+void	write_error(char *msg)
+{
+	write(STDERR_FILENO, msg, ft_strlen(msg));
+}
+
 int	main(int argc, char **argv)
 {
+	if (argc != 5 && argc != 6)
+	{
+		write_error("Usage: ./pipex <arg1> <arg2> <arg3> <arg4> [<arg5>]\n");
+		write_error("<arg1>: number_of_philosophers\n");
+		write_error("<arg2>: time to die\n");
+		write_error("<arg3>: time to eat\n");
+		write_error("<arg4>: time to sleep\n");
+		write_error("[<arg5>]: number_of_times_each_philosopher_must_eat\n");
+		exit(EXIT_FAILURE);
+	}
 }
