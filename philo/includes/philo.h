23b62cea77e6e821e3689bc19c077348da4af5c1
ychng <ychng@student.42kl.edu.my>
2023-11-19 18:44:03 +0800

    redo setup

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 14:08:29 by ychng             #+#    #+#             */
-/*   Updated: 2023/11/18 22:03:57 by ychng            ###   ########.fr       */
+/*   Updated: 2023/11/19 18:42:50 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,27 @@
 # include "sys_headers.h"
 # include "libft.h"
 
+typedef struct s_philo_stats
+{
+	pthread_mutex_t	*forks;
+}	t_philo_stats;
+
 typedef struct s_philo_config
 {
 	size_t				number_of_philosophers;
+	size_t				number_of_times_to_eat;
 	unsigned long long	time_to_die;
 	unsigned long long	time_to_eat;
 	unsigned long long	time_to_sleep;
-	size_t				number_of_times_to_eat;
 }	t_philo_config;
 
-typedef struct s_dining_philo
+typedef struct s_philo_info
 {
-	size_t			*philosophers;
-	size_t			*chopsticks;
-	t_philo_config	config;
-}	t_dining_philo;
+	size_t			position;
+	size_t			num_of_times_ate;
+	t_philo_stats	*shared_stats;
+	t_philo_config	*shared_config;
+}	t_philo_info;
 
 // philo.c
 void	write_error(const char *msg);
@@ -46,8 +52,5 @@ void	set_philo_config(t_philo_config *config, char *token, size_t i);
 
 // philo_requirements.c
 bool	meets_philo_requirements(t_philo_config *config);
-
-// exec_threads.c
-void	exec_threads(t_dining_philo *dp);
 
 #endif
--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 14:07:56 by ychng             #+#    #+#             */
-/*   Updated: 2023/11/18 22:03:37 by ychng            ###   ########.fr       */
+/*   Updated: 2023/11/19 18:43:37 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,9 @@ void	write_error(const char *msg)
 	write(STDERR_FILENO, msg, ft_strlen(msg));
 }
 
-void	init_dining_philo(t_dining_philo *dp)
-{
-	dp->chopsticks = NULL;
-	dp->philosophers = NULL;
-	dp->config = (t_philo_config){0};
-}
-
 int	main(int argc, char **argv)
 {
-	t_dining_philo	dp;
+	t_philo_config	config;
 
 	if (argc != 5 && argc != 6)
 	{
@@ -38,7 +31,5 @@ int	main(int argc, char **argv)
 		write_error("[<arg5>]: number_of_times_each_philosopher_must_eat\n");
 		exit(EXIT_FAILURE);
 	}
-	init_dining_philo(&dp);
-	parse_argv(&dp.config, argv);
-	exec_threads(&dp);
+	parse_argv(&config, argv);
 }
