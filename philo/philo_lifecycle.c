b534958ffe26d324d13027a9743931bbe2edf36d
Yuen Zheng Chng <ychng@u90z01s02.42kl.edu.my>
2023-12-29 01:51:52 +0800

    fix more than 25 lines in write_activity

--- philo/philo_lifecycle.c
+++ philo/philo_lifecycle.c
@@ -6,7 +6,7 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 22:17:12 by ychng             #+#    #+#             */
-/*   Updated: 2023/12/29 01:08:40 by ychng            ###   ########.fr       */
+/*   Updated: 2023/12/29 01:50:49 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,36 +23,40 @@ size_t	get_elapsed_time(struct timeval start_time)
 	return (diff_in_ms);
 }
 
+static void	print_activity(t_philo_info *philo, char *activity,
+	struct timeval start_time)
+{
+	if (ft_strcmp(activity, "fork") == 0)
+	{
+		printf("%ld %ld has taken a fork\n",
+			get_elapsed_time(start_time),
+			philo->position
+			);
+	}
+	else if (ft_strcmp(activity, "died") == 0)
+	{
+		printf("%ld %ld died\n",
+			get_elapsed_time(start_time),
+			philo->position
+			);
+		philo->shared_stats->stop_printing = true;
+	}
+	else
+	{
+		printf("%ld %ld is %s\n",
+			get_elapsed_time(start_time),
+			philo->position, activity
+			);
+	}
+}
+
 void	write_activity(t_philo_info *philo, char *activity,
 	struct timeval start_time)
 {
 	pthread_mutex_lock(philo->shared_stats->log_mutex);
 	pthread_mutex_lock(philo->shared_stats->stop_printing_mutex);
 	if (!philo->shared_stats->stop_printing)
-	{
-		if (ft_strcmp(activity, "fork") == 0)
-		{
-			printf("%ld %ld has taken a fork\n",
-				get_elapsed_time(start_time),
-				philo->position
-				);
-		}
-		else if (ft_strcmp(activity, "died") == 0)
-		{
-			printf("%ld %ld died\n",
-				get_elapsed_time(start_time),
-				philo->position
-				);
-			philo->shared_stats->stop_printing = true;
-		}
-		else
-		{
-			printf("%ld %ld is %s\n",
-				get_elapsed_time(start_time),
-				philo->position, activity
-				);
-		}
-	}
+		print_activity(philo, activity, start_time);
 	pthread_mutex_unlock(philo->shared_stats->stop_printing_mutex);
 	pthread_mutex_unlock(philo->shared_stats->log_mutex);
 }
