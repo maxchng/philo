eeb0b0908a6293ad94be138b63940311b1f39e0f
Yuen Zheng Chng <ychng@u91z01s03.42kl.edu.my>
2024-01-11 22:36:43 +0800

    remove comment in bonus

--- philo_bonus/start_kill.c
+++ philo_bonus/start_kill.c
@@ -6,29 +6,11 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 16:49:16 by ychng             #+#    #+#             */
-/*   Updated: 2024/01/11 22:35:39 by ychng            ###   ########.fr       */
+/*   Updated: 2024/01/11 22:36:31 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/philo_bonus.h"
-
-// static bool	check_exit_condition(t_philo_info *philo)
-// {
-// 	size_t	num_of_times_to_eat;
-// 	size_t	eating_counter;
-// 	bool	stop_printing;
-
-// 	num_of_times_to_eat = philo->shared_config->num_of_times_to_eat;
-// 	sem_wait(philo->shared_stats->eating_counter_sem);
-// 	eating_counter = philo->eating_counter;
-// 	sem_post(philo->shared_stats->eating_counter_sem);
-// 	sem_wait(philo->shared_stats->stop_printing_sem);
-// 	stop_printing = philo->shared_stats->stop_printing;
-// 	sem_post(philo->shared_stats->stop_printing_sem);
-// 	if (stop_printing)
-// 		return (true);
-// 	return (num_of_times_to_eat != 0 && eating_counter >= num_of_times_to_eat);
-// }
 
 void	*start_kill(void *arg)
 {
