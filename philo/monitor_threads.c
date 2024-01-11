6771d33947a2dc1430df1dbc9319f58ad836de2a
Yuen Zheng Chng <ychng@u91z01s03.42kl.edu.my>
2024-01-11 21:07:58 +0800

    ok manadatory done

--- philo/monitor_threads.c
+++ philo/monitor_threads.c
@@ -6,43 +6,11 @@
 /*   By: ychng <ychng@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 02:25:23 by ychng             #+#    #+#             */
-/*   Updated: 2024/01/11 18:49:03 by ychng            ###   ########.fr       */
+/*   Updated: 2024/01/11 21:06:03 by ychng            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/philo.h"
-
-// static bool	check_exit_condition(t_philo_info *philo)
-// {
-// 	size_t	num_of_times_to_eat;
-// 	size_t	eating_counter;
-// 	bool	stop_printing;
-
-// 	num_of_times_to_eat = philo->shared_config->num_of_times_to_eat;
-// 	pthread_mutex_lock(philo->shared_stats->eating_counter_mutex);
-// 	eating_counter = philo->eating_counter;
-// 	pthread_mutex_unlock(philo->shared_stats->eating_counter_mutex);
-// 	pthread_mutex_lock(philo->shared_stats->stop_printing_mutex);
-// 	stop_printing = philo->shared_stats->stop_printing;
-// 	pthread_mutex_unlock(philo->shared_stats->stop_printing_mutex);
-// 	if (stop_printing)
-// 		return (true);
-// 	return (num_of_times_to_eat != 0 && eating_counter >= num_of_times_to_eat);
-// }
-
-// static bool	should_exit(t_philo_info *philos, size_t num_of_philos)
-// {
-// 	size_t	i;
-
-// 	i = 0;
-// 	while (i < num_of_philos)
-// 	{
-// 		if (check_exit_condition(&philos[i]))
-// 			return (true);
-// 		i++;
-// 	}
-// 	return (false);
-// }
 
 static int	check_philo_last_meal_time(t_philo_info	*philos)
 {
@@ -64,11 +32,50 @@ static int	check_philo_last_meal_time(t_philo_info	*philos)
 		if (time_since_last_meal > philos[0].shared_config->time_to_die)
 		{
 			write_activity(&philos[i], "died", start_time);
-			return (-1);
+			return (true);
 		}
 		i++;
 	}
-	return (0);
+	return (false);
+}
+
+static bool	check_exit_condition(t_philo_info *philo)
+{
+	size_t	num_of_times_to_eat;
+	size_t	eating_counter;
+	bool	stop_printing;
+
+	num_of_times_to_eat = philo->shared_config->num_of_times_to_eat;
+	pthread_mutex_lock(philo->shared_stats->eating_counter_mutex);
+	eating_counter = philo->eating_counter;
+	pthread_mutex_unlock(philo->shared_stats->eating_counter_mutex);
+	pthread_mutex_lock(philo->shared_stats->stop_printing_mutex);
+	stop_printing = philo->shared_stats->stop_printing;
+	pthread_mutex_unlock(philo->shared_stats->stop_printing_mutex);
+	if (stop_printing)
+		return (true);
+	return (num_of_times_to_eat != 0 && eating_counter >= num_of_times_to_eat);
+}
+
+static bool	check_eating_counter(t_philo_info *philos)
+{
+	size_t	i;
+	size_t	num_of_philos;
+
+	i = 0;
+	num_of_philos = philos[0].shared_config->num_of_philos;
+	while (i < num_of_philos)
+	{
+		if (check_exit_condition(&philos[i]))
+		{
+			pthread_mutex_lock(philos[0].shared_stats->stop_printing_mutex);
+			philos[0].shared_stats->stop_printing = true;
+			pthread_mutex_unlock(philos[0].shared_stats->stop_printing_mutex);
+			return (true);
+		}
+		i++;
+	}
+	return (false);
 }
 
 void	monitor_threads(void *arg)
@@ -78,7 +85,9 @@ void	monitor_threads(void *arg)
 	philos = (t_philo_info *)arg;
 	while (1)
 	{
-		if (check_philo_last_meal_time(philos) == -1)
+		if (check_philo_last_meal_time(philos))
+			return ;
+		else if (check_eating_counter(philos))
 			return ;
 		usleep(50);
 	}
